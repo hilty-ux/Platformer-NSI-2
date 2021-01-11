4e3c5068baa3d896c122f636118856802dd5f901
ƒkS124 <57041516+hilty-ux@users.noreply.github.com>
2021-01-11 11:46:17 +0100

    Add files via upload

--- ground.py
+++ ground.py
@@ -3,19 +3,22 @@ import pygame
 
 class Ground(pygame.sprite.Sprite):
     
+    """Hérite de la super classe Sprite de pygame, cela va permettre de faire des groupes de sprites notamment"""
+    
     def __init__(self, screen, W, H):
         
-        super().__init__()
+        super().__init__() # initialise la super classe
         
         self.screen = screen
         self.W = W
         self.H = H
         
-        self.rect = pygame.Rect((0, self.H - 100), (W, H))
+        self.rect = pygame.Rect((0, self.H - 100), (W, H)) # définit un rectangle
         
-        self.color = (0, 200, 0)
+        self.color = (0, 200, 0) # définit une couleur
         
     def update(self):
         
+        # déssine un rectangle sur la fenetre : 'self.screen', dans la couleur: 'self.color', dans le rectangle : 'self.rect'
         pygame.draw.rect(self.screen, self.color, self.rect)
     
