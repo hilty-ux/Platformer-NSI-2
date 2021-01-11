556669af3eeb53a752e2b5742a02cbd5df568673
ƒkS124 <57041516+hilty-ux@users.noreply.github.com>
2021-01-11 17:25:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pygame
-import game
+import game # import le fichier game
 
 
-W = 1800
-H = 1000
-screen = pygame.display.set_mode((W, H))
+W = 1800 # définit une variable désignant la largeur de l'écran
+H = 1000 # définit une variable désignant la hauteur de l'écran
+screen = pygame.display.set_mode((W, H)) # crée l'écran avec pour largeur W et hauteur H
 
-game = game.Game(screen, W, H)
+game = game.Game(screen, W, H) # initialise la classe Game provenant du fichier game
 
-pygame.init()
+pygame.init() # initialise pygame
 
-game.main_loop()
+game.main_loop() # appelle la fonction main_loop de la classe Game, elle désigne les boucles de jeu
 
-pygame.quit()
+pygame.quit() # quitte pygame, quand la boucle de jeu est finie/cassée
             
             
             
